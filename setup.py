0684f7bef6490d3f1d83b7609ed485f1531b4fa1
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-03-16 16:07:21 -0400

    Pull latest pflog and pftag

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'markimg',
-    version          = '1.1.22',
+    version          = '1.1.24',
     description      = 'An app to mark landmark points and lines on an input image',
     long_description = readme,
     author           = 'FNNDSC',
